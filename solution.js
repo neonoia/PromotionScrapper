37775b34236a674cc7c894d1234753966458a2c1
Hafizh Budiman <hafizh.budiman@gmail.com>
2018-04-09 14:37:11 +0700

    Fixed double promotion

--- solution.js
+++ solution.js
@@ -10,8 +10,8 @@ var dict = {};
 
 var scrape_url = "https://m.bnizona.com/index.php/category/index/promo";
 
-function scrapeCategory(url){
-    return request(url).then(function(html){
+function scrapeCategory(url) {
+    return request(url).then(function (html) {
         category = [];
         var $ = cheerio.load(html);
 
@@ -21,47 +21,48 @@ function scrapeCategory(url){
                 'link': $(elem).find('a').attr('href'),
                 'title': $(elem).find('a').text()
             });
-            dict[title] = [];
         });
         return category;
     });
 }
 
 function scrapeDetail(category) {
-    return request(category.link).then(function(html) {
+    return request(category.link).then(function (html) {
+        var title = category.title;
         var $ = cheerio.load(html);
+        dict[title] = [];
+        var listDetail = [];
 
         $('.list2').find('li').each(function (idx, body) {
             var detail = $(body).find('a');
-            var title = category.title;
             dict[title].push({
+                'promoTitle': $(detail).children('.promo-title').text(),
                 'link': $(detail).attr('href'),
                 'thumbnail': $(detail).find('img').attr('src'),
                 'merchantName': $(detail).children('.merchant-name').text(),
-                'promoTitle': $(detail).children('.promo-title').text(),
                 'validUntil': $(detail).children('.valid-until').text()
             });
-        });	
-        return dict;
+        });
+        return listDetail;
     });
 }
 
 function scrape(url) {
-    return scrapeCategory(url).then(function(category) {
+    return scrapeCategory(url).then(function (category) {
         return Promise.map(category, scrapeDetail);
     });
 }
 
 function main(url) {
-    scrape(url).then(function(result) {
-        var json = JSON.stringify(result);
+    scrape(url).then(function (result) {
+        var json = JSON.stringify(dict);
         fs.writeFile('result.json', json, 'utf8', function (err) {
             if (err) {
                 return console.log(err);
             }
 
             console.log("Promotions successfully saved to result.json.");
-        }); 
+        });
     })
 }
 
